785e7048503fd725f81672442557c7222db7ab73
Hugo Posnic <hugo@fintecture.com>
2022-06-21 12:02:46 +0200

    1.2.14-M2.3

--- Model/Fintecture.php
+++ Model/Fintecture.php
@@ -35,7 +35,7 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class Fintecture extends AbstractMethod
 {
-    private const MODULE_VERSION = '1.2.13';
+    private const MODULE_VERSION = '1.2.14';
     public const PAYMENT_FINTECTURE_CODE = 'fintecture';
     public const CONFIG_PREFIX = 'payment/fintecture/';
 
