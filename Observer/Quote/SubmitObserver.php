dc58351d0ca44c85240c209654aba03cc7147cf7
Hugo Posnic <hugo@fintecture.com>
2022-07-29 14:14:54 +0200

    1.4.0-M2.3

--- Observer/Quote/SubmitObserver.php
+++ Observer/Quote/SubmitObserver.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace Fintecture\Payment\Observer\Quote;
 
 use Fintecture\Payment\Model\Fintecture;
+use Magento\Sales\Model\Order;
 
 class SubmitObserver
 {
     public function beforeExecute($subject, $observer)
     {
+        /** @var Order $order */
         $order = $observer->getEvent()->getOrder();
 
-        $paymentMethod = $order->getPayment()->getData('method');
-        if ($paymentMethod === Fintecture::CODE) {
-            // Disable email sending
-            $order->setCanSendNewEmailFlag(false);
+        if ($order) {
+            if ($order->getPayment()->getMethod() === Fintecture::CODE) {
+                // Disable email sending
+                $order->setCanSendNewEmailFlag(false);
+            }
         }
 
         return [$observer];
