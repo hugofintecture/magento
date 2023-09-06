45a7de58ddd27005cfe1e04e48e49486b5a53af6
Hugo Posnic <hugo@fintecture.com>
2023-09-06 14:27:47 +0200

    3.2.0

--- Model/Config/Options/CheckoutDesignSelectionOptions.php
+++ Model/Config/Options/CheckoutDesignSelectionOptions.php
@@ -9,8 +9,8 @@ class CheckoutDesignSelectionOptions implements \Magento\Framework\Data\OptionSo
         return [
             ['value' => 'it', 'label' => __('Immediate Transfer')],
             ['value' => 'ist', 'label' => __('Immediate Transfer & Smart Transfer')],
-            ['value' => 'ist_long', 'label' => __('Long Version')],
-            ['value' => 'ist_short', 'label' => __('Short Version')],
+            ['value' => 'ist_long', 'label' => __('Long version')],
+            ['value' => 'ist_short', 'label' => __('Short version')],
         ];
     }
 }
